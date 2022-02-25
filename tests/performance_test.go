8f2ca3f06773028bbb069cc9bb31bf3a8c9e3a29
foxfurry <isacartur@gmail.com>
2022-02-25 18:09:20 +0200

    Added performance tests

--- tests/performance_test.go
+++ tests/performance_test.go
@@ -1,7 +1,29 @@
 package tests
 
-import "testing"
+import (
+	"KeyValueHTTPStore/internal/cluster"
+	"KeyValueHTTPStore/internal/command"
+	"context"
+	"runtime"
+	"testing"
+)
 
-func Test_Performance(t *testing.T) {
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
 
+func Benchmark_Performance(t *testing.B) {
+	ctx, cancel := context.WithCancel(context.Background())
+	storage := cluster.New()
+	storage.Initialize(ctx)
+
+	cmds := []command.Command{
+		command.Set("test", "testv"),
+	}
+
+	for i := 0; i < t.N; i++ {
+		storage.Execute(cmds)
+	}
+
+	cancel()
 }
