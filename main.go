a496fd5d438f0a66aa58423a4b98b60f8213100f
foxfurry <isacartur@gmail.com>
2022-03-02 14:44:15 +0200

    Main with sample test

--- main.go
+++ main.go
@@ -14,28 +14,46 @@ func init() {
 }
 
 func main() {
+
+	// Init
 	ctx, cancel := context.WithCancel(context.Background())
 	storage := cluster.New()
 	storage.Initialize(ctx)
+	time.Sleep(time.Nanosecond * 10000) // Let all goroutines start and get ready
 
-	cmds := []command.Command{
-		command.Set("test", "testv"),
+	const N = 10
+	var totalTime time.Duration
+	for i := 0; i < N; i++ {
+
+		// Execute set
+		cmds := []command.Command{
+			command.Set("foo", "bar"),
+		}
+		result, _ := storage.Execute(cmds)
+
+		// Execute get
+		cmds = []command.Command{
+			command.Get("foo"),
+		}
+
+		setExec := time.Now() // Record time
+		for {
+			result, _ = storage.Execute(cmds)
+
+			if result != nil && result[0] == "bar" {
+				break
+			}
+		}
+
+		elapsed := time.Since(setExec)
+		totalTime += elapsed
+
+		fmt.Printf("Try #%d: %s\n", i, elapsed)
 	}
 
-	result, err := storage.Execute(cmds)
-
-	time.Sleep(time.Nanosecond * 500)
-
-	fmt.Printf("Res: %v\n------\nErr: %v\n\n\n", result, err)
-
-	cmds = []command.Command{
-		command.Get("test"),
-	}
-
-	result, err = storage.Execute(cmds)
-	fmt.Printf("Res: %v\n------\nErr: %v\n\n", result, err)
-
-	time.Sleep(time.Nanosecond * 10000)
+	fmt.Printf("Total time: %s\nAverage time between set and get: %s", totalTime, totalTime/N)
 
 	cancel()
+	time.Sleep(time.Nanosecond * 10000)
+
 }
