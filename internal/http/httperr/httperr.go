71aa2d76689d5f951a8a73853ecaea73bb41a0af
foxfurry <isacartur@gmail.com>
2022-03-30 11:31:23 +0300

    Reworked project structure for http part

---
+++ internal/http/httperr/httperr.go
@@ -0,0 +1,26 @@
+package server
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+func handleError(c *gin.Context, err error, statusCode int) {
+	c.JSON(statusCode, gin.H{
+		"error": err,
+	})
+}
+
+func InternalError(c *gin.Context, err error) {
+	handleError(c, err, http.StatusInternalServerError)
+}
+
+func BadRequest(c *gin.Context, err error) {
+	handleError(c, err, http.StatusBadRequest)
+}
+
+func Wrap(err error, msg string) error {
+	return errors.New(fmt.Sprintf("%s: %v", msg, err))
+}
